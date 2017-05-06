8ddfae189d46b14b4f13c5b77d6c89532a214f0d
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2017-05-07 01:01:11 +0800

    add j-lyric.net support

---
+++ lyric_engine_js/modules/j_lyric.js
@@ -0,0 +1,62 @@
+const util = require('util');
+const iconv = require('iconv-lite');
+const he = require('he');
+const rp = require('request-promise');
+const striptags = require('striptags');
+
+const LyricBase = require('../include/lyric_base');
+const keyword = 'j-lyric.net';
+
+class Lyric extends LyricBase {
+    async find_lyric(url, html) {
+        const prefix = "<p id='lyricBody'>";
+        const suffix = '</p>';
+
+        let lyric = this.find_string_by_prefix_suffix(html, prefix, suffix, false);
+        if (!lyric) {
+            console.error('Failed to find lyric of url:', url);
+            return false;
+        }
+
+        lyric = he.decode(lyric);
+        lyric = striptags(lyric);
+        lyric = lyric.trim();
+
+        this.lyric = lyric;
+        return true;
+    }
+
+    async find_info(url, html) {
+        const patterns = {
+            'title': "<div class='caption'><h2>(.*?)</h2></div>",
+            'artist': '<p>歌：(.*?)</p>',
+            'lyricist': '<p>作詞：(.*?)</p>',
+            'composer': '<p>作曲：(.*?)</p>',
+        }
+
+        this.fill_song_info(html, patterns);
+    }
+
+    async parse_page() {
+        const url = this.url;
+
+        const html = await rp(url);
+
+        await this.find_lyric(url, html);
+        await this.find_info(url, html);
+
+        return true;
+    }
+}
+
+exports.keyword = keyword;
+exports.Lyric  = Lyric;
+
+if (require.main === module) {
+    (async function() {
+        const url = 'http://j-lyric.net/artist/a04cc4b/l020e9b.html';
+        const obj = new Lyric(url);
+        const lyric =  await obj.get();
+        console.log(lyric);
+    })();
+}
