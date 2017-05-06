dca809a3293f64c7875da8ba5c1be445cfc09181
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2017-05-07 00:48:12 +0800

    add music-book.jp support

---
+++ lyric_engine_js/modules/music_jp.js
@@ -0,0 +1,105 @@
+const URL = require('url');
+const util = require('util');
+const iconv = require('iconv-lite');
+const he = require('he');
+
+const rp = require('request-promise');
+const striptags = require('striptags');
+
+const LyricBase = require('../include/lyric_base');
+const keyword = 'music-book.jp';
+
+class Lyric extends LyricBase {
+    async get_artist_id(url) {
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2141.0 Safari/537.36'
+        };
+        const html = await rp({
+            method: 'POST',
+            uri:url,
+            headers: headers
+        });
+
+        const pattern = "checkFavorite\\('([0-9]+)'\\)";
+        const id = this.get_first_group_by_pattern(html, pattern);
+        return id;
+    }
+
+    async get_song_json(url) {
+        const artist_id = await this.get_artist_id(url);
+        if (!artist_id) {
+            console.error('Failed to get artist id of url:', url);
+            return false;
+        }
+
+        console.log('artist_id is', artist_id);
+
+        const url_object = URL.parse(url, true);
+
+        const pos = url_object.pathname.lastIndexOf('/');
+        const pid = url_object.pathname.substr(pos + 1);
+
+        console.log('pid:', pid);
+
+        const post_url = 'http://music-book.jp/music/MusicDetail/GetLyric';
+        const body = {
+            'artistId': artist_id,
+            'artistName': decodeURIComponent(url_object.query.artistname),
+            'title': (url_object.query.title),
+            'muid': '',
+            'pid': pid,
+            'packageName': decodeURIComponent(url_object.query.packageName),
+        };
+
+        const json = await rp({
+            method: 'POST',
+            uri: post_url,
+            form: body,
+            json: true,
+        })
+
+        return json;
+    }
+
+    async find_lyric(url, json) {
+        let lyric = json.Lyrics;
+
+        lyric = lyric.replace(/<br \/>/g, '\n');
+        lyric = lyric.trim();
+
+        this.lyric = lyric;
+        return true;
+    }
+
+    async find_info(url, json) {
+        const url_object = URL.parse(url, true);
+
+        this.title = decodeURIComponent(url_object.query.title);
+        this.artist = decodeURIComponent(url_object.query.artistname);
+        this.lyricist = json.Writer;
+        this.composer = json.Composer;
+    }
+
+    async parse_page() {
+        const url = this.url;
+
+        const json = await this.get_song_json(url);
+
+        await this.find_lyric(url, json);
+        await this.find_info(url, json);
+
+        return true;
+    }
+}
+
+exports.keyword = keyword;
+exports.Lyric  = Lyric;
+
+if (require.main === module) {
+    (async function() {
+        const url = 'http://music-book.jp/music/Kashi/aaa6rh9s?artistname=%25e5%2580%2589%25e6%259c%25a8%25e9%25ba%25bb%25e8%25a1%25a3&title=%25e6%25b8%25a1%25e6%259c%2588%25e6%25a9%258b%2520%25ef%25bd%259e%25e5%2590%259b%2520%25e6%2583%25b3%25e3%2581%25b5%25ef%25bd%259e&packageName=%25e6%25b8%25a1%25e6%259c%2588%25e6%25a9%258b%2520%25ef%25bd%259e%25e5%2590%259b%2520%25e6%2583%25b3%25e3%2581%25b5%25ef%25bd%259e';
+        const obj = new Lyric(url);
+        const lyric =  await obj.get();
+        console.log(lyric);
+    })();
+}
