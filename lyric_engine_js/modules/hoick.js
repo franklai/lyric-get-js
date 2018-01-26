54a99e338c39a457f7673076e88b9ede2a7869a1
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2018-01-26 17:15:55 +0800

    Add hoick.jp

---
+++ lyric_engine_js/modules/hoick.js
@@ -0,0 +1,64 @@
+const rp = require('request-promise');
+
+const LyricBase = require('../include/lyric_base');
+
+const keyword = 'hoick';
+
+class Lyric extends LyricBase {
+  find_id(url) {
+    const pattern = '/detail/([0-9]+)/';
+    return this.get_first_group_by_pattern(url, pattern);
+  }
+
+  async find_lyric(url) {
+    const id = this.find_id(url);
+
+    const lyric_url = `https://hoick.jp/data.php?id=${id}&type=4`;
+    const lines = await rp({ uri: lyric_url, json: true });
+
+    lines.pop(); // last one is a number
+
+    const lyric = lines.join('\n');
+
+    this.lyric = lyric;
+
+    return true;
+  }
+
+  async find_info(url) {
+    const html = await rp(url);
+
+    const prefix = '<meta property="og:description"';
+    const suffix = '/>';
+    const og_desc = this.find_string_by_prefix_suffix(html, prefix, suffix, false);
+
+    const patterns = {
+      title: 'content="(.*?)\\(詞:',
+      lyricist: '作詞：(.*?)/作曲：',
+      composer: '作曲：(.*?)\\)"',
+    };
+
+    this.fill_song_info(og_desc, patterns);
+  }
+
+  async parse_page() {
+    const { url } = this;
+
+    await this.find_lyric(url);
+    await this.find_info(url);
+
+    return true;
+  }
+}
+
+exports.keyword = keyword;
+exports.Lyric = Lyric;
+
+if (require.main === module) {
+  (async () => {
+    const url = 'http://hoick.jp/mdb/detail/9920/%E3%81%AB%E3%81%98';
+    const obj = new Lyric(url);
+    const lyric = await obj.get();
+    console.log(lyric);
+  })();
+}
---
+++ lyric_engine_js/modules/hoick.test.js
@@ -0,0 +1,35 @@
+/* global expect test */
+const { Lyric } = require('./hoick');
+
+async function testLyric(obj) {
+  const { url } = obj;
+  const inst = new Lyric(url);
+  await inst.get();
+
+  expect(inst.title).toBe(obj.title);
+  expect(inst.artist).toBe(obj.artist);
+  if (obj.lyricist) expect(inst.lyricist).toBe(obj.lyricist);
+  if (obj.composer) expect(inst.composer).toBe(obj.composer);
+  if (obj.arranger) expect(inst.arranger).toBe(obj.arranger);
+  if (obj.length) expect(inst.lyric.length).toBe(obj.length);
+}
+
+test('', async () => {
+  await testLyric({
+    url: 'http://hoick.jp/mdb/detail/9920/%E3%81%AB%E3%81%98',
+    title: 'にじ',
+    lyricist: '新沢としひこ',
+    composer: '中川ひろたか',
+    length: 509,
+  });
+});
+
+test('', async () => {
+  await testLyric({
+    url: 'http://hoick.jp/mdb/detail/19471/%E3%83%93%E3%83%BC%E3%81%A0%E3%81%BE%E3%83%93%E3%83%BC%E3%81%99%E3%81%91%E3%81%AE%E5%A4%A7%E5%86%92%E9%99%BA',
+    title: 'ビーだまビーすけの大冒険',
+    lyricist: '佐藤雅彦,内野真澄',
+    composer: '栗原正己',
+    length: 275,
+  });
+});
