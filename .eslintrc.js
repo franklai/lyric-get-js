9a0220fea5ddf101f78d61efccc5b06c59f4c079
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2020-08-09 16:44:19 +0800

    upgrade eslint and superagent; adjust eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "class-methods-use-this": "off",
-        "camelcase": "off",
-        "no-console": "off",
-        "no-throw-literal": "off",
-        "comma-dangle": ["error", "only-multiline"],
-    },
-    "env": {
-        "browser": true,
-    },
-    "settings": {
-        "import/core-modules": ["electron"],
-    }
+  extends: 'airbnb-base',
+  rules: {
+    'class-methods-use-this': 'off',
+    camelcase: 'off',
+    'no-console': 'off',
+    'no-throw-literal': 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+    'operator-linebreak': ['error', 'after'],
+  },
+  env: {
+    browser: true,
+  },
+  settings: {
+    'import/core-modules': ['electron'],
+  },
 };
