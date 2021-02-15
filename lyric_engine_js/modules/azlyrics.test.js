10ce17a5521310ecae436f13acad6f6e0ec1d14c
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2021-02-15 09:14:11 +0800

    add azlyrics

---
+++ lyric_engine_js/modules/azlyrics.test.js
@@ -0,0 +1,33 @@
+/* global expect test */
+const { Lyric } = require('./azlyrics');
+const BlockedError = require('../include/blocked-error');
+
+async function testLyric(object) {
+  const { url, title, artist, lyricist, composer, arranger, length } = object;
+  const inst = new Lyric(url);
+  await inst.get();
+
+  expect(inst.title).toBe(title);
+  expect(inst.artist).toBe(artist);
+  if (lyricist) expect(inst.lyricist).toBe(lyricist);
+  if (composer) expect(inst.composer).toBe(composer);
+  if (length > 0) expect(inst.lyric.length).toBe(length);
+}
+
+test('', async () => {
+  await testLyric({
+    url: 'https://www.azlyrics.com/lyrics/coldplay/adventureofalifetime.html',
+    title: 'Adventure Of A Lifetime',
+    artist: 'Coldplay',
+    length: 1154,
+  });
+});
+
+test('', async () => {
+  await testLyric({
+    url: 'https://www.azlyrics.com/lyrics/gunsnroses/sweetchildomine.html',
+    title: "Sweet Child O' Mine",
+    artist: "Guns N' Roses",
+    length: 1056,
+  });
+});
