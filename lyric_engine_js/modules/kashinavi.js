cdfd6b54ab2db255af00fd30b2c21cf44308aafe
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2022-04-15 07:33:59 +0800

    fix eslint

--- lyric_engine_js/modules/kashinavi.js
+++ lyric_engine_js/modules/kashinavi.js
@@ -12,7 +12,7 @@ class Lyric extends LyricBase {
     const prefix = 'oncopy="return false;" unselectable="on;">';
     const suffix = '</div>';
     let lyric = this.find_string_by_prefix_suffix(html, prefix, suffix, false);
-    lyric = lyric.replace(/<br>　<br>/g, '<br><br>');
+    lyric = lyric.replace(/<br>　<br>/g, '<br><br>'); // eslint-disable-line no-irregular-whitespace
     lyric = lyric.replace(/<\/p><p>/g, '\n\n');
     lyric = lyric.replace(/<br>/g, '\n');
     lyric = this.sanitize_html(lyric);
