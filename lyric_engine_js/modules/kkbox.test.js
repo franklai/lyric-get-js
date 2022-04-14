3d038023a8fdbf8694d13e155517b01212fdc176
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2022-04-15 07:39:01 +0800

    [kkbox] add 20s timeout for test

--- lyric_engine_js/modules/kkbox.test.js
+++ lyric_engine_js/modules/kkbox.test.js
@@ -1,6 +1,8 @@
-/* global expect test */
+/* global expect jest test */
 const { Lyric } = require('./kkbox');
 const BlockedError = require('../include/blocked-error');
+
+jest.setTimeout(20_000); // 20 second timeout
 
 async function testLyric(object) {
   const { url, title, artist, lyricist, composer, arranger, length } = object;
