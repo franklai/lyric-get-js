b8ee92660a6604c17128d34df484483a377c7966
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2022-07-31 23:14:03 +0800

    cloudflare use azlyrics

--- functions/api/lyric/get/[url].js
+++ functions/api/lyric/get/[url].js
@@ -1,4 +1,6 @@
-const engine = require('../../../../lyric_engine_js');
+// const engine = require('../../../../lyric_engine_js');
+// eslint-disable-next-line global-require
+const sites = [require('../../../../lyric_engine_js/modules/azlyrics')];
 
 // eslint-disable-next-line import/prefer-default-export
 export async function onRequestGet({ params }) {
@@ -6,8 +8,18 @@ export async function onRequestGet({ params }) {
   const out = {
     lyric: `Failed to find lyric of ${url}`,
   };
+
+  console.log(`url: ${url}`);
+
   try {
-    const lyric = await engine.get_full(url);
+    const site = sites.find((item) => url.includes(item.keyword));
+
+    const object = new site.Lyric(url);
+    if (!(await object.parse_page())) {
+      throw new Error('Parse failed.');
+    }
+
+    const lyric = object.get_full();
     out.lyric = lyric;
   } catch (error) {
     console.log(`something wrong, error: ${error}`);
