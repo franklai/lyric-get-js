e22afa6f86df1188a6d85a2a6a118d7284186863
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2025-03-22 16:21:42 +0800

    refactor: use fetch in petitlyrics

--- lyric_engine_js/modules/petitlyrics.js
+++ lyric_engine_js/modules/petitlyrics.js
@@ -1,4 +1,3 @@
-const superagent = require('superagent');
 const LyricBase = require('../include/lyric-base');
 
 const keyword = 'petitlyrics';
@@ -9,21 +8,22 @@ class Lyric extends LyricBase {
     return this.get_first_group_by_pattern(html, pattern);
   }
 
-  get_1st_part(html) {
-    const prefix = '<canvas ';
-    const suffix = '</canvas>';
-    let lyric = this.find_string_by_prefix_suffix(html, prefix, suffix, true);
-    if (!lyric) {
-      console.warn('Failed to find lyric');
-      return false;
-    }
+  async get_html_and_plsession(url) {
+    const resp = await fetch(url);
+    const cookie = resp.headers.get('set-cookie');
+    const plsession = this.get_first_group_by_pattern(
+      cookie,
+      /PLSESSION=([a-z0-9]+);/
+    );
 
-    lyric = this.sanitize_html(lyric);
-
-    return lyric;
+    const html = await resp.text();
+    return {
+      html,
+      plsession,
+    };
   }
 
-  async get_csrf_token(html) {
+  async get_csrf_token(html, plsession) {
     const pattern = /(\/lib\/pl-lib.js[^"]+)/;
     const library_pl_js = this.get_first_group_by_pattern(html, pattern);
     if (!library_pl_js) {
@@ -34,46 +34,48 @@ class Lyric extends LyricBase {
     const site_url = 'https://petitlyrics.com';
     const js_url = `${site_url}${library_pl_js}`;
 
-    const response = await this.agent.get(js_url);
-    const js_raw = response.text;
+    const resp = await fetch(js_url, {
+      headers: {
+        cookie: `PLSESSION=${plsession}`,
+      },
+    });
 
+    const js_raw = await resp.text();
     if (!js_raw) {
       console.warn('Failed to get js content, url:', js_url);
       return false;
     }
 
     const token_pattern = /'X-CSRF-Token', '(.*?)'/;
-    const token = this.get_first_group_by_pattern(js_raw, token_pattern);
+    const csrf_token = this.get_first_group_by_pattern(js_raw, token_pattern);
 
-    return token;
+    return csrf_token;
   }
 
-  async get_2nd_part(html) {
-    const song_id = this.find_song_id(html);
-    // console.log('song_id:', song_id);
-
-    const csrf_token = await this.get_csrf_token(html);
-    if (!csrf_token) {
-      console.warn('Failed to get CSRF token');
-      return false;
-    }
-    // console.log('csrf token:', csrf_token);
-
+  async get_lyrics_by_ajax(song_id, plsession, csrf_token) {
     const lyric_url = 'https://petitlyrics.com/com/get_lyrics.ajax';
     const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
       'X-CSRF-Token': csrf_token,
       'X-Requested-With': 'XMLHttpRequest',
+      cookie: `PLSESSION=${plsession}`,
     };
     const body = `lyrics_id=${song_id}`;
 
-    const response = await this.agent.post(lyric_url).set(headers).send(body);
-
-    if (!response) {
-      console.warn('Failed to get lyrics using post');
-      return false;
+    const resp = await fetch(lyric_url, {
+      method: 'POST',
+      headers,
+      body,
+    });
+    if (resp.status !== 200) {
+      console.warn(`failed to get response, status: ${resp.status}`);
+      console.log(resp);
+      return '';
     }
 
-    const items = response.body;
+    const text = await resp.text();
+    const items = JSON.parse(text);
+
     const lyric = items
       .map((item) => Buffer.from(item.lyrics, 'base64'))
       .join('\n');
@@ -81,8 +83,8 @@ class Lyric extends LyricBase {
     return lyric;
   }
 
-  async find_lyric(url, html) {
-    const lyric = await this.get_2nd_part(html);
+  async find_lyric(song_id, plsession, csrf_token) {
+    const lyric = await this.get_lyrics_by_ajax(song_id, plsession, csrf_token);
 
     this.lyric = lyric.trim();
     return true;
@@ -130,14 +132,18 @@ class Lyric extends LyricBase {
   }
 
   async parse_page() {
-    this.agent = superagent.agent();
+    const { url } = this;
 
-    const { agent, url } = this;
+    const { html, plsession } = await this.get_html_and_plsession(url);
+    // console.log('plsession:', plsession);
 
-    const response = await agent.get(url);
-    const html = response.text;
+    const csrf_token = await this.get_csrf_token(html, plsession);
+    // console.log('csrf_token:', csrf_token);
 
-    await this.find_lyric(url, html);
+    const song_id = this.find_song_id(html);
+    // console.log('song_id:', song_id);
+
+    await this.find_lyric(song_id, plsession, csrf_token);
     await this.find_info(url, html);
 
     return true;
@@ -151,7 +157,7 @@ if (require.main === module) {
   (async () => {
     //         const url = 'https://petitlyrics.com/lyrics/2640215';
     //         const url = 'https://petitlyrics.com/lyrics/2664168';
-    const url = 'https://petitlyrics.com/lyrics/2664256';
+    const url = 'https://petitlyrics.com/lyrics/1209279';
     const object = new Lyric(url);
     const lyric = await object.get();
     console.log(lyric);
