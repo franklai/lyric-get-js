f9b52e2073e4624e76d6eb8293689fb608c467a3
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2025-10-05 09:05:41 +0800

    fix: remove debug log for lyrical-nonsense

--- lyric_engine_js/modules/lyrical-nonsense.js
+++ lyric_engine_js/modules/lyrical-nonsense.js
@@ -55,19 +55,10 @@ class Lyric extends LyricBase {
         : '</div><div class="ln-row-cont">';
     }
 
-    console.warn(`== get block ==`);
-    console.warn(`== url: ${url}, content id: ${content_id}, is global: ${is_global} ==`);
-    
     const block = this.find_string_by_prefix_suffix(html, prefix, suffix);
     if (block) {
-      console.warn(`== found block ==`);
-      console.warn(block);
-
       return block;
     }
-
-    console.warn(`== NOT found block ==`);
-    console.warn(html);
 
     return this.find_string_by_prefix_suffix(html, prefix, suffix);
   }
